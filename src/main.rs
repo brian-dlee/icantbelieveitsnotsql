94a87a275b5c05575172359de05d5191b081445f
Brian Lee <briandl92391@gmail.com>
2025-10-29 08:58:08 -0700

    parse sqlite tables

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use clap::Parser;
-use sqlparser::ast::Statement;
+use sqlparser::ast::{ObjectName, Statement};
 use sqlparser::dialect::GenericDialect;
 use sqlparser::parser::{Parser as SQLParser, ParserError};
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, Read};
+use std::iter::Map;
 
 #[derive(Parser)]
 struct Args {
@@ -48,14 +50,45 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         },
         Ok(ast) => {
+            let mut tables: HashMap<String, HashMap<String, String>> = HashMap::new();
+
             for statement in ast {
                 match statement {
                     Statement::CreateTable(create_table) => {
-                        eprintln!("")
+                        fn extract_ident_from_object_name(
+                            object_name: &ObjectName,
+                        ) -> Result<String, String> {
+                            let mut name_object_name_parts = object_name.0.iter();
+                            let size = name_object_name_parts.clone().count();
+
+                            if size != 1 {
+                                Err(format!(
+                                    "Unsupported ObjectName parts count: expected 1, got {}",
+                                    size
+                                ))
+                            } else {
+                                let part = name_object_name_parts.next().unwrap();
+                                let ident = part.as_ident().unwrap();
+
+                                Ok(ident.value.clone())
+                            }
+                        }
+
+                        let table_name = extract_ident_from_object_name(&create_table.name)?;
+
+                        let mut columns: HashMap<String, String> = HashMap::new();
+
+                        for column in create_table.columns.iter() {
+                            columns.insert(column.name.value.clone(), column.data_type.to_string());
+                        }
+
+                        tables.insert(table_name, columns);
                     }
                     _ => {}
                 }
             }
+
+            println!("{:#?}", tables);
         }
     }
 
